51ef6ff3f075bac7d0ce8a5ef20af6b360af6815
breder <andbreder@gmail.com>
2021-09-24 22:22:48 -0300

    cronometro

--- holyrics.js
+++ holyrics.js
@@ -1,6 +1,6 @@
 const debug = true
 const holyricsServer = 'http://192.168.1.54/stage-view/text.json'
-const usedAnimations = 'animate__fadeIn animate__fadeInDown animate__fadeInRight animate__fadeInUp animate__flipInX animate__fadeOut animate__fadeOutDown animate__fadeOutRight animate__fadeOutUp animate__flipOutX'
+const usedAnimations = 'animate__fadeIn animate__fadeInUp animate__fadeInRight animate__fadeInDown animate__fadeInLeft animate__fadeOut animate__fadeOutUp animate__fadeOutRight animate__fadeOutDown animate__fadeOutLeft animate__flipInX animate__flipOutX'
 
 function log(a) {
   if (!debug)
@@ -25,6 +25,80 @@ function getAnimateCssDelay() {
 
 var app = {
   bible: {
+    versicleAnimation: 'NEXT',
+    opened: {
+      book: '',
+      chapter: '',
+      versicle: '',
+    },
+    order: [
+      "Gênesis",
+      "Êxodo",
+      "Levítico",
+      "Números",
+      "Deuteronômio",
+      "Josué",
+      "Juízes",
+      "Rute",
+      "1 Samuel",
+      "2 Samuel",
+      "1 Reis",
+      "2 Reis",
+      "1 Crônicas",
+      "2 Crônicas",
+      "Esdras",
+      "Neemias",
+      "Ester",
+      "Jó",
+      "Salmos",
+      "Provérbios",
+      "Eclesiastes",
+      "Cantares",
+      "Isaías",
+      "Jeremias",
+      "Lamentações",
+      "Ezequiel",
+      "Daniel",
+      "Oséias",
+      "Joel",
+      "Amós",
+      "Obadias",
+      "Jonas",
+      "Miquéias",
+      "Naum",
+      "Habacuque",
+      "Sofonias",
+      "Ageu",
+      "Zacarais",
+      "Malaquias",
+      "Mateus",
+      "Marcos",
+      "Lucas",
+      "João",
+      "Atos",
+      "Romanos",
+      "1 Coríntios",
+      "2 Coríntios",
+      "Gálatas",
+      "Efésios",
+      "Filipenses",
+      "Colossenses",
+      "1 Tessalonicenses",
+      "2 Tessalonicenses",
+      "1 Timóteo",
+      "2 Timóteo",
+      "Tito",
+      "Filemon",
+      "Hebreus",
+      "Tiago",
+      "1 Pedro",
+      "2 Pedro",
+      "1 João",
+      "2 João",
+      "3 João",
+      "Judas",
+      "Apocalipse"
+    ],
     data: {
       header: null,
       text: null,
@@ -37,7 +111,7 @@ var app = {
       translation: function() { return $('.bible .translation') },
     },
     close: function() {
-      log('app.bible.close()')
+      //log('app.bible.close()')
       app.bible.data = {
         header: null,
         text: null,
@@ -46,7 +120,7 @@ var app = {
       app.close('.bible','animate__fadeOut', 'animate__fadeIn')
     },
     show: function(holyricsRawData) {
-      log('app.bible.show()')
+      //log('app.bible.show()')
       var data
       try {
         data = {
@@ -60,8 +134,8 @@ var app = {
         log(holyricsRawData)
         throw ex
       }
-      log("app.bible.data ({0}) EQUALS data ({1})", JSON.stringify(app.bible.data), JSON.stringify(data))
-      log('RESULT <<<<<  {0}  >>>>>', _.isEqual(app.bible.data, data))
+      // log("app.bible.data ({0}) EQUALS data ({1})", JSON.stringify(app.bible.data), JSON.stringify(data))
+      // log('RESULT <<<<<  {0}  >>>>>', _.isEqual(app.bible.data, data))
       if (_.isEqual(app.bible.data, data))
         return
       logstatus('app.bible.show() <= "show versicle"')
@@ -69,11 +143,36 @@ var app = {
         '<p class="header animate__animated">'+data.header+'</p>'+
         '<p class="text animate__animated">'+data.text+'</p>'+
         '<p class="translation animate__animated">'+data.translation+'</p>'
-      app.bible.append(content)
+      let headerMatch = data.header.match(/(.*)\s(\d+)\:(\d+)/)
+      let opened = { 
+        book: headerMatch[1], 
+        chapter: headerMatch[2], 
+        versicle: headerMatch[3] 
+      }
+      if (opened.book != app.bible.opened.book) {
+        for (let i = 0; i < app.bible.order.length; i++) {
+          if (opened.book === app.bible.order[i]) {
+            app.bible.versicleAnimation = (app.bible.order.indexOf(app.bible.opened.book) < i) ? 'NEXT' : 'BACK'
+            break
+          }
+        }
+      } else {
+        if (app.bible.opened.chapter === opened.chapter) {
+          app.bible.versicleAnimation = (app.bible.opened.versicle > opened.versicle) ? 'BACK' : 'NEXT'
+          console.log(`${app.bible.opened.versicle} > ${opened.versicle}`)
+          console.log(`VERSICLE -- ${app.bible.versicleAnimation}`)
+        } else {
+          app.bible.versicleAnimation = (app.bible.opened.chapter > opened.chapter) ? 'BACK' : 'NEXT'
+          console.log(`${app.bible.opened.chapter} > ${opened.chapter}`)
+          console.log(`chapter -- ${app.bible.versicleAnimation}`)
+        }
+      }
+      app.bible.opened = opened
       app.bible.data = data
+      app.bible.append(content)
     },
     append: function(e) {
-      log('app.bible.append()')
+      //log('app.bible.append()')
       if (app.lyrics.get.container().is(':visible'))
         app.lyrics.get.container().hide()
       var container = app.bible.get.container()
@@ -86,11 +185,12 @@ var app = {
           .addClass('animate__fadeOut')
         $($(e)[0]).addClass('animate__fadeIn').appendTo(container)
 
+        console.log(`app.bible.versicleAnimation == ${app.bible.versicleAnimation}`)
         var text = app.bible.get.text()
         text
           .removeClass(usedAnimations)
-          .addClass('animate__fadeOutLeft')
-          $($(e)[1]).addClass('animate__fadeInRight').appendTo(container)
+          .addClass(app.bible.versicleAnimation === 'NEXT' ? 'animate__fadeOutLeft' : 'animate__fadeOutRight')
+          $($(e)[1]).addClass(app.bible.versicleAnimation === 'NEXT' ? 'animate__fadeInRight' : 'animate__fadeInLeft').appendTo(container)
 
         var translation = app.bible.get.translation()
         translation
@@ -119,7 +219,7 @@ var app = {
       verse:     function() { return $('.music .verse') },
     },
     close: function() {
-      log('app.lyrics.close()')
+      //log('app.lyrics.close()')
       app.lyrics.data = {
         title: null,
         artist: null,
@@ -128,7 +228,7 @@ var app = {
       app.close('.music','animate__fadeOutDown', 'animate__fadeInUp')
     },
     show: function(holyricsRawData) {
-      log('app.lyrics.show()')
+      //log('app.lyrics.show()')
       if (app.bible.get.container().is(':visible'))
         app.bible.get.container().hide()
       var data = {
@@ -173,7 +273,7 @@ var app = {
       }
     },
     append: function(e) {
-      log('app.lyrics.append()')
+      //log('app.lyrics.append()')
       var container = app.lyrics.get.container()
       if (container.is(":hidden")) {
         container.html(null).append(e).show()
@@ -195,11 +295,22 @@ var app = {
       }
     }
   },
+  text: {
+    show: function(holyricsRawData) {
+      //log('app.lyrics.show()')
+      var crono = holyricsRawData.text.match(/^(\d+\:\d+)\<span/)
+      $('.container-crono > p').html(crono[1])
+      if ($('.container-crono').is(":hidden"))
+        $('.container-crono').show()
+      if (crono[1].match(/^(0+\:0+)$/))
+        $('.container-crono').hide()
+    },
+  },
   //
   // generic functions
   //
   close: function(e, an_out, an_in) {
-    log('app.lyrics.close()')
+    //log('app.lyrics.close()')
     var element = $(e)
     element.removeClass(usedAnimations)
     element.addClass(an_out)
@@ -230,6 +341,10 @@ $(function() {
         case 'BIBLE':
           logstatus('holyrics.types.bible')
           app.bible.show(response.map)
+          break;
+        case 'TEXT':
+          logstatus('holyrics.types.text')
+          app.text.show(response.map)
           break;
         default:
           throw "exception: unhandled 'holyrics.type' = " + response.map.type
